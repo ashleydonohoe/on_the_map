87693d92a390b3eef175027de850ad98e6910c49
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-08-25 22:39:01 -0400

    finished UdacityConstants for now

--- On The Map/UdacityConstants.swift
+++ On The Map/UdacityConstants.swift
@@ -25,8 +25,21 @@ extension UdacityClient {
         static let Session = "/session"
     }
     
-    // Parameter keys
-    struct ParameterKeys {
-        
+    // JSON Body Keys
+    struct JSONBodyKeys {
+        static let Username = "username"
+        static let Password = "password"
+        static let UdacityDictionary = "udacity"
     }
+    
+    // JSON Response Keys
+    struct JSONResponseKeys {
+        static let Account = "account"
+        static let Session = "session"
+        static let User = "user"
+        static let UserKey = "key"
+        static let FirstName = "first_name"
+        static let LastName = "last_name"
+    }
+    
 }
