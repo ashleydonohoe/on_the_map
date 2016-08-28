38703a1d1e1eaccb99b2e99ec3eb0a4210cbd979
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-08-28 15:46:56 -0400

    testing table with fake data

--- On The Map/TableViewController.swift
+++ On The Map/TableViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var studentInfoTable: UITableView!
+    var students = [[String: AnyObject]]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let student1 = ["name": "Ashley", "location": "Cincinnati"]
+        let student2 = ["name": "John", "location": "New York City"]
+        
+        students = [student1, student2]
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +33,18 @@ class TableViewController: UIViewController {
         print("logging out")
         let controller = self.storyboard?.instantiateViewControllerWithIdentifier("UdacityLoginViewController") as! LoginViewController
         self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("StudentInfoCell")! as UITableViewCell
+        let student = students[indexPath.row]
+        cell.textLabel!.text = student["name"] as! String
+        cell.detailTextLabel?.text = student["location"] as! String
+        return cell
     }
 
 }
